f608899204d74b85f8e1fef41a5e24f434026be3
Artur Garin <artur.garin@lamoda.ru>
2026-07-02 22:57:45 +0300

    Initial tap: review-reminder cask

---
+++ Casks/review-reminder.rb
@@ -0,0 +1,29 @@
+cask "review-reminder" do
+  version "0.2.0"
+  sha256 "PLACEHOLDER_SHA256"
+
+  url "https://github.com/garinag/review-reminder/releases/download/v#{version}/ReviewReminder.zip"
+  name "Review Reminder"
+  desc "Трей-приложение для macOS, напоминает про открытые GitLab merge request'ы"
+  homepage "https://github.com/garinag/review-reminder"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "ReviewReminder.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                    args: ["-rd", "com.apple.quarantine", "#{appdir}/ReviewReminder.app"],
+                    sudo: false
+  end
+
+  zap trash: [
+    "~/Library/Application Support/ReviewReminder",
+    "~/Library/Preferences/com.reviewreminder.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,24 @@
+# garinag/homebrew-tap
+
+Homebrew tap для приложений [garinag](https://github.com/garinag).
+
+## Установка
+
+```bash
+brew tap garinag/tap
+brew install --cask review-reminder
+```
+
+Обновление:
+
+```bash
+brew upgrade --cask review-reminder
+```
+
+## Доступные пакеты
+
+| Cask | Описание |
+|------|----------|
+| `review-reminder` | Трей-приложение macOS, напоминает про открытые GitLab merge request'ы. [Репозиторий](https://github.com/garinag/review-reminder) |
+
+Cask-файл обновляется автоматически при каждом релизе `review-reminder` (см. `.github/workflows/release.yml` в основном репозитории).
